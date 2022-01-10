a50302e826f736a0db661411e9c82096b6e65b40
kytwb <kytwb@pm.me>
2022-01-10 21:10:40 +0100

    Add test notification trigger

---
+++ public/notification.js
@@ -0,0 +1,21 @@
+function sendTestNotification(title = "Test notification title", body = "Test notification body", icon){
+  if (!("Notification" in window)) {
+    console.warn("Browser does not support notifications")
+  }
+  else if (Notification.permission === "granted") {
+    new Notification(title, { body, icon });
+  }
+  else if (Notification.permission !== 'denied') {
+    Notification.requestPermission((permission) => {
+      if (!('permission' in Notification)) {
+        Notification.permission = permission;
+      }
+      if (permission === "granted") {
+        new Notification(title, { body, icon });
+      }
+    });
+  }
+  else {
+    console.warn(`Failed, notification permission is ${Notification.permission}`);
+  }
+}
